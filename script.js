f5831ee09408729ae91459cb20294627a71edf5b
Redney Monteiro <79022289+RedneyMonteiro15@users.noreply.github.com>
2021-05-02 19:57:35 +0100

    finalizando a parte de javascript na reserva

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+var valido = true
 function enviar()
 {
-    var nome = document.getElementById('nome')
-    var email = document.getElementById('email')
+    var nome = document.getElementById('nome').value
+    var email = document.getElementById('email').value
+    var dataEntrada = window.document.getElementById('dataEntrada').value
+    var dataSaida = window.document.getElementById('dataSaida').value
+    var adulto = Number(window.document.getElementById('adulto').value)
+    var crianca = Number(window.document.getElementById('crianca').value)
     var res = document.getElementById('res')
-    res.innerText = `${nome} ${email}`
+    res.style.color = '#d62828'
+    if(nome.length == 0 || email.length == 0 || dataEntrada.length == 0 || dataSaida.length == 0 || adulto.length == 0)
+    {
+        res.innerText = 'Erro!!! Por favor preenche todos os campos'
+    }
+    else if(!valido)
+    {
+        res.innerText = 'Verifica o seu email'
+    }
+    else
+    {
+        var total = (adulto * 50) + (crianca * 30)
+        res.style.color = "green"
+        res.innerText = `Preço Total: ${total}€`
+    }
+}
+function validarEmail(){
+    var email = document.querySelector('#email');
+    var res = document.getElementById('valEmail')
+    if(!email.checkValidity()){
+        res.style.color = '#d62828'
+        res.style.marginTop = '-20px'
+        res.innerHTML = "Email invalido";  
+        valido = false
+    }
+    else
+    {
+        res.innerText = ''
+        valido = true
+    }
 }
